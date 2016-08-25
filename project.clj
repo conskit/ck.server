6021a43e0e4bec80f2dfa129a83d6393ab93ee0d
Jason Murphy <json.murphy@gmail.com>
2016-08-24 19:48:44 -0500

    Conskit and Project version bump

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject ck.server "0.2.0-SNAPSHOT"
+(defproject ck.server "1.0.0-rc1"
   :description "Server module for Conskit"
   :url "https://github.com/conskit/ck.server"
   :license {:name "Apache License, Version 2.0"
             :url "http://www.apache.org/licenses/LICENSE-2.0.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [conskit "0.2.0-SNAPSHOT"]]
+                 [conskit "1.0.0-rc1"]]
   :profiles {:dev {:source-paths ["dev" "http-kit"]
                    :dependencies [[puppetlabs/trapperkeeper "1.4.1" :classifier "test"]
                                   [puppetlabs/kitchensink "1.3.1" :classifier "test" :scope "test"]
